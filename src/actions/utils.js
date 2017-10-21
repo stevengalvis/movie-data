911a2999f914b1874bffbde1db4dd64a162db90e
stevengalvis <steven.galvis@gmail.com>
2017-10-21 16:46:48 -0400

    added user action for adding users

---
+++ src/actions/utils.js
@@ -0,0 +1,13 @@
+// handling errors from the API
+export const normalizeResponseErrors = res => {
+  if (!res.ok) {
+    if (res.headers.has("content-type") && res.headers.get("content-type").startsWith("application/json")) {
+      return res.json().then(err => Promise.reject(err));
+    }
+    return Promise.reject({
+      code: res.status,
+      message: res.statusText
+    });
+  }
+  return res;
+};
