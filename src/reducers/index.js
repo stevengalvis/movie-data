b6d3e8e719f79cfd249aeb0c82b3ef889bc58541
stevengalvis <steven.galvis@gmail.com>
2017-10-21 20:16:38 -0400

    added input component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import movie from "./movie";
 import favorites from "./favorites";
+import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
   movie,
-  favorites
+  favorites,
+  form: formReducer
 });
 
 export default rootReducer;
