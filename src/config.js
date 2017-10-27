bf1accf95804e9069dbdb77a3a56f47b354ed6ac
stevengalvis <steven.galvis@gmail.com>
2017-10-26 20:29:28 -0400

    fixed searchbox bug

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 import { normalizeResponseErrors } from "./actions/utils";
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+export const MOVIE_DB_API_KEY = process.env.MOVIE_DB_API_KEY || "f0fd8b59eca23a619a92fb9c86cfaf23";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import movie from "./movie";
 import favorites from "./favorites";
 import authReducer from "./auth";
 import { reducer as formReducer } from "redux-form";
+import searchReducer from "./search";
 
 const rootReducer = combineReducers({
   movie,
   favorites,
   form: formReducer,
-  auth: authReducer
+  auth: authReducer,
+  search: searchReducer
 });
 
 export default rootReducer;
